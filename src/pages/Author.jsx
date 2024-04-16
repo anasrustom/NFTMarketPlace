9bb970652910eab39f79220e0bb4d5665155e0ce
Anas <123774736+anasrustom@users.noreply.github.com>
2024-04-16 22:54:08 +0300

    Add files via upload

---
+++ src/pages/Author.jsx
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import AuthorBanner from "../images/author_banner.jpg";
+import AuthorItems from "../components/author/AuthorItems";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+const Author = () => {
+  const [author, setAuthor] = useState();
+  const [loading, setLoading] = useState();
+  const [follow, setFollow] = useState(false);
+  const [followers, setFollowers] = useState(0);
+  const { id } = useParams();
+  function followUser() {
+    setFollow((prev) => !prev);
+  }
+  async function fetchPosts() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`
+    );
+    setAuthor(data);
+    setLoading(true);
+    setFollowers(author.followers + 1);
+  }
+  fetchPosts();
+
+  return (
+    <div id="wrapper">
+      {loading && (
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+
+          <section
+            id="profile_banner"
+            aria-label="section"
+            className="text-light"
+            data-bgimage="url(images/author_banner.jpg) top"
+            style={{ background: `url(${AuthorBanner}) top` }}
+          ></section>
+
+          <section aria-label="section">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <img src={author.authorImage} alt="" />
+
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {author.authorName}
+                            <span className="profile_username">
+                              {author.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {author.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile_follow de-flex">
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {follow ? (
+                            <>{followers} </>
+                          ) : (
+                            <> {author.followers} </>
+                          )}
+                          followers
+                        </div>
+                        <Link to="#" className="btn-main" onClick={followUser}>
+                          {follow ? <>Following</> : <>Follow</>}
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems
+                      collection={author.nftCollection}
+                      authorImage={author.authorImage}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Author;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
+import BrowseByCategory from "../components/home/BrowseByCategory";
+import HotCollections from "../components/home/HotCollections";
 import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
+import NewItems from "../components/home/NewItems";
+import TopSellers from "../components/home/TopSellers";
 
 const Home = () => {
   useEffect(() => {
@@ -13,6 +17,10 @@ const Home = () => {
         <div id="top"></div>
         <Landing />
         <LandingIntro />
+        <HotCollections />
+        <NewItems />
+        <TopSellers />
+        <BrowseByCategory />
       </div>
     </div>
   );
