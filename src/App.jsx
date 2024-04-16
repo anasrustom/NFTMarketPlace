bc3e0beb4916d2ebc4a8f3a07a2bad17aa955620
Anas <123774736+anasrustom@users.noreply.github.com>
2024-04-16 22:53:41 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import Home from "./pages/Home";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Explore from "./pages/Explore";
-import baj from "./pages/baj";
-import Author1 from "./pages/Author1";
-import Author2 from "./pages/Author2";
-import Author3 from "./pages/Author3";
-import Author4 from "./pages/Author4";
-import Author5 from "./pages/Author5";
-import Author6 from "./pages/Author6";
+import Author from "./pages/Author";
+import ItemDetails from "./pages/ItemDetails";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
@@ -18,13 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
-        <Route path="/baj" element={<baj />} />
-        <Route path="/author1" element={<Author1 />} />
-        <Route path="/author2" element={<Author2 />} />
-        <Route path="/author3" element={<Author3 />} />
-        <Route path="/author4" element={<Author4 />} />
-        <Route path="/author5" element={<Author5 />} />
-        <Route path="/author6" element={<Author6 />} />
+        <Route path="/author/:id" element={<Author />} />
+        <Route path="/item-details/:id" element={<ItemDetails />} />
       </Routes>
       <Footer />
     </Router>
